dc8114ce68b4d57bee11375be1894bbc5209af15
sanjeevani_kandra_09 <sanjeevanikandra24@navgurukul.org>
2024-11-08 09:49:44 +0530

    Update LogIn.js

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -21,7 +21,7 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/auth/login', credentials);
+            const response = await axios.post('https://expense-tracker-backend-2-2mum.onrender.com/api/auth/login', credentials);
             alert('Login successful!');
             console.log(response.data);
             
