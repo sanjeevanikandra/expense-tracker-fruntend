887954add51b045548d69a51ce86d1acd8b5d5d5
sanjeevani_kandra_09 <sanjeevanikandra24@navgurukul.org>
2024-11-08 09:48:21 +0530

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 export const fetchExpenses = async () => {
-  const response = await axios.get('http://localhost:5000/api/expenses');
+  const response = await axios.get('https://expense-tracker-backend-2-2mum.onrender.com/api/expenses');
   return response.data;
 };
 
 export const addExpense = async (newExpense) => {
-  const response = await axios.post('http://localhost:5000/api/expenses', newExpense);
+  const response = await axios.post('https://expense-tracker-backend-2-2mum.onrender.com/api/expenses', newExpense);
   return response.data;
 };
 
 export const updateExpense = async (updatedExpense) => {
-  const response = await axios.put(`http://localhost:5000/api/expenses/${updatedExpense._id}`, updatedExpense);
+  const response = await axios.put(`https://expense-tracker-backend-2-2mum.onrender.com/api/expenses/${updatedExpense._id}`, updatedExpense);
   return response.data;
 };
 
 export const deleteExpense = async (id) => {
-  await axios.delete(`http://localhost:5000/api/expenses/${id}`);
+  await axios.delete(`https://expense-tracker-backend-2-2mum.onrender.com/api/expenses/${id}`);
 };
